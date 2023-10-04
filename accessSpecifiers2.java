2eb499039db15be5afcadb7b3009b33b2973e484
Kgaogelo Sehlakwane Tshehla <TshehlaKS@tut.ac.za>
2023-10-04 21:28:49 +0200

    Changes have been committed.

---
+++ accessSpecifiers2.java
@@ -0,0 +1,19 @@
+class priaccessspecifier 
+{ 
+   private void display() 
+    { 
+        System.out.println("You are using private access specifier"); 
+    } 
+} 
+
+public class accessSpecifiers2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//private
+		System.out.println("Private Access Specifier");
+		priaccessspecifier  obj = new priaccessspecifier(); 
+		//obj.display();
+	}
+
+}
